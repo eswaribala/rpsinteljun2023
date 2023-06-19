a98eaedce29564412602da7beb6d04f39d4bdda7
eswaribala <parameswaribala@gmail.com>
2023-06-19 11:34:31 +0530

    updated

--- InventoryAPI/InventoryAPI/Repositories/ProductRepo.cs
+++ InventoryAPI/InventoryAPI/Repositories/ProductRepo.cs
@@ -1,32 +1,85 @@
-﻿using InventoryAPI.Models;
+﻿using InventoryAPI.Contexts;
+using InventoryAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace InventoryAPI.Repositories
 {
     public class ProductRepo : IProductRepo
     {
-        public Task<Product> AddProduct(Product Product, long CategoryId)
+        private InventoryContext _context;
+
+        public ProductRepo(InventoryContext context) { 
+         _context = context;
+        }
+        public async Task<Product> AddProduct(Product Product, long CategoryId)
         {
-            throw new NotImplementedException();
+            Category? result = await this._context.Categories.FirstOrDefaultAsync(c =>
+         c.CategoryId == CategoryId);
+
+            if (result != null)
+            {
+                Product.Category = result;
+                var productResult = await this._context.Products.AddAsync(Product);
+                await this._context.SaveChangesAsync();
+                return productResult.Entity;
+            }
+            else
+                return null;
+            
+
+
         }
 
-        public Task<bool> DeleteProduct(long ProductId)
+        public async Task<bool> DeleteProduct(long ProductId)
         {
-            throw new NotImplementedException();
+            var result = await this._context.Products.FirstOrDefaultAsync(p =>
+           p.ProductId == ProductId);
+
+
+            if (result != null)
+            {
+                this._context.Products.Remove(result);
+                await this._context.SaveChangesAsync();
+            }
+
+            result = await this._context.Products.FirstOrDefaultAsync(p =>
+            p.ProductId == ProductId);
+            if (result == null)
+                return true;
+            else
+                return false;
         }
 
-        public Task<Product> GetProductById(long ProductId)
+        public async Task<Product> GetProductById(long ProductId)
         {
-            throw new NotImplementedException();
+            var result = await this._context.Products.FirstOrDefaultAsync(p =>
+           p.ProductId == ProductId);
+            if (result != null)
+                return result;
+            else
+                return null;
+
+
         }
 
-        public Task<IEnumerable<Product>> GetProducts()
+        public async Task<IEnumerable<Product>> GetProducts()
         {
-            throw new NotImplementedException();
+            return await this._context.Products.ToListAsync();
         }
 
-        public Task<Product> UpdateProduct(long ProductId, string ProductName)
+        public async Task<Product> UpdateProduct(long ProductId, string ProductName)
         {
-            throw new NotImplementedException();
+            var result = await this._context.Products.FirstOrDefaultAsync(p =>
+         p.ProductId == ProductId);
+            if (result != null)
+            {
+                result.ProductName = ProductName;
+                await this._context.SaveChangesAsync();
+                return result;
+
+            }
+            else
+                return null;
         }
     }
 }
