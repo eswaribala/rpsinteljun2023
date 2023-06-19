7b79052b8fa2d6416084095d2e3e12b51f2b20c6
eswaribala <parameswaribala@gmail.com>
2023-06-19 11:21:53 +0530

    updated

---
+++ InventoryAPI/InventoryAPI/Repositories/CategoryRepo.cs
@@ -0,0 +1,76 @@
+﻿using InventoryAPI.Contexts;
+using InventoryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryAPI.Repositories
+{
+    public class CategoryRepo : ICategoryRepo
+    {
+        private InventoryContext _context;
+
+        public CategoryRepo(InventoryContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Category> AddCategory(Category category)
+        {
+            var result = await this._context.Categories.AddAsync(category);
+
+            await this._context.SaveChangesAsync();
+
+            return result.Entity;
+
+        }
+
+        public async Task<bool> DeleteCategory(long CategoryId)
+        {
+           var result=  await this._context.Categories.FirstOrDefaultAsync(c =>
+            c.CategoryId == CategoryId);
+            if(result != null)
+            {
+                this._context.Categories.Remove(result);
+                await  this._context.SaveChangesAsync();
+            }
+
+           result = await this._context.Categories.FirstOrDefaultAsync(c =>
+           c.CategoryId == CategoryId);
+            if (result == null)
+                return true;
+            else
+                return false;
+
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            return await this._context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> GetCategoryById(long CategoryId)
+        {
+            var result = await this._context.Categories.FirstOrDefaultAsync(c =>
+           c.CategoryId == CategoryId);
+            if (result != null)
+                return result;
+            else
+                return null;
+        }
+
+        public async Task<Category> UpdateCategory(long CategoryId, string CategoryName)
+        {
+            var result = await this._context.Categories.FirstOrDefaultAsync(c =>
+           c.CategoryId == CategoryId);
+            if (result != null)
+            {
+                result.CategoryName = CategoryName;
+                await this._context.SaveChangesAsync();
+                return result;
+
+            }
+            else
+                return null;
+
+        }
+    }
+}
---
+++ InventoryAPI/InventoryAPI/Repositories/ProductRepo.cs
@@ -0,0 +1,32 @@
+﻿using InventoryAPI.Models;
+
+namespace InventoryAPI.Repositories
+{
+    public class ProductRepo : IProductRepo
+    {
+        public Task<Product> AddProduct(Product Product, long CategoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProduct(long ProductId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductById(long ProductId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> UpdateProduct(long ProductId, string ProductName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
