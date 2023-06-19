989a0ed46159688d7615e95d64dc0e4a5bed45d4
eswaribala <parameswaribala@gmail.com>
2023-06-19 09:22:24 +0530

    updated

---
+++ InventoryAPI/InventoryAPI/Contexts/InventoryContext.cs
@@ -0,0 +1,17 @@
+﻿using InventoryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryAPI.Contexts
+{
+    public class InventoryContext:DbContext
+    {
+        public InventoryContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            this.Database.EnsureCreated();
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
