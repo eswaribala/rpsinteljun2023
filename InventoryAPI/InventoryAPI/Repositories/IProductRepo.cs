c307bc41164319cd8e503c660c775c321d2abff8
eswaribala <parameswaribala@gmail.com>
2023-06-19 09:58:11 +0530

    updated

---
+++ InventoryAPI/InventoryAPI/Repositories/IProductRepo.cs
@@ -0,0 +1,13 @@
+﻿using InventoryAPI.Models;
+
+namespace InventoryAPI.Repositories
+{
+    public interface IProductRepo
+    {
+        Task<Product> AddProduct(Product Product, long CategoryId);
+        Task<Product> UpdateProduct(long ProductId, string ProductName);
+        Task<bool> DeleteProduct(long ProductId);
+        Task<Product> GetProductById(long ProductId);
+        Task<IEnumerable<Product>> GetProducts();
+    }
+}
