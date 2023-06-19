ddefd9f4d06fb86deb0f188a43bee3630fff6847
eswaribala <parameswaribala@gmail.com>
2023-06-19 09:00:32 +0530

    updated

--- InventoryAPI/InventoryAPI/Models/Product.cs
+++ InventoryAPI/InventoryAPI/Models/Product.cs
@@ -1,6 +1,28 @@
-﻿namespace InventoryAPI.Models
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InventoryAPI.Models
 {
+    [Table("Product")]
     public class Product
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("ProductId",TypeName ="bigint")]
+        public long ProductId { get; set; }
+        [Column("ProductName", TypeName = "varchar(200")]
+        public string ProductName { get; set; }=string.Empty;
+        //value object
+        public ProductDescription? productDescription { get; set; }
+        [Column("SKU", TypeName = "varchar(200")]
+        public string SKU { get; set; }= string.Empty;
+        [ForeignKey("Category")]
+        [Column("Category_Id_FK")]
+        public long CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+
     }
 }
---
+++ InventoryAPI/InventoryAPI/Models/ProductDescription.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace InventoryAPI.Models
+{
+    [Owned]
+    public class ProductDescription
+    {
+        [Column("PurchasedDate")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM dd yyyy}")]
+        public DateTime PurchasedDate { get; set; }
+        [Column("ExpiryDate")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM dd yyyy}")]
+        public DateTime ExpiryDate { get; set; }
+        [Column("Cost",TypeName="bigint")]
+        public long Cost { get; set; }
+        [Column("BufferLevel", TypeName = "bigint")]
+        public long BufferLevel { get; set; }
+
+    }
+}
