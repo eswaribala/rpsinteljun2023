7724e7b21601308a633b2aad7912c5c5c1f75652
eswaribala <parameswaribala@gmail.com>
2023-06-20 09:26:10 +0530

    updated

--- InventoryAPI/InventoryAPI/Mutations/RootMutation.cs
+++ InventoryAPI/InventoryAPI/Mutations/RootMutation.cs
@@ -50,6 +50,80 @@ namespace InventoryAPI.Mutations
             });
 
 
+            FieldAsync<StringGraphType>("deleteCategory",
+            arguments: new QueryArguments(
+
+                new QueryArgument<LongGraphType> { Name = "categoryId" }
+             
+
+                ),
+
+            resolve: async context =>
+            {
+
+                var categoryId = context.GetArgument<long>("categoryId");
+             
+                await DeleteCategory(categoryId);
+                return $"CategoryId {categoryId} is successfully deleted";
+            });
+
+            //product
+
+
+            FieldAsync<ProductGLType>("insertProduct",
+            arguments: new QueryArguments(
+
+                new QueryArgument<ProductGLInputType> { Name = "product" },
+                new QueryArgument<LongGraphType> { Name = "categoryId" }
+
+                ),
+
+            resolve: async context =>
+            {
+
+                var product = context.GetArgument<Product>("product");
+                var categoryId = context.GetArgument<long>("categoryId");
+                var result = await InsertProduct(categoryId,product);
+                return result;
+            });
+
+            FieldAsync<ProductGLType>("updateProduct",
+            arguments: new QueryArguments(
+
+                new QueryArgument<LongGraphType> { Name = "productId" },
+                new QueryArgument<StringGraphType> { Name = "productName" }
+
+                ),
+
+            resolve: async context =>
+            {
+
+                var productId = context.GetArgument<long>("productId");
+                var productName = context.GetArgument<string>("productName");
+                var result = await UpdateProduct(productId, productName);
+                return result;
+            });
+
+
+            FieldAsync<StringGraphType>("deleteProduct",
+            arguments: new QueryArguments(
+
+                new QueryArgument<LongGraphType> { Name = "productId" }
+
+
+                ),
+
+            resolve: async context =>
+            {
+
+                var productId = context.GetArgument<long>("productId");
+
+                await DeleteProduct(productId);
+                return $"ProductId {productId} is successfully deleted";
+            });
+
+
+
         }
 
 
@@ -69,6 +143,49 @@ namespace InventoryAPI.Mutations
             else
             {
                 return await _categoryRepo.UpdateCategory(categoryId, categoryName);
+            }
+        }
+
+        private async Task<bool> DeleteCategory(long categoryId)
+        {
+            if (categoryId <= 0)
+                return false;
+            else
+            {
+                return await _categoryRepo.DeleteCategory(categoryId);
+            }
+        }
+
+
+
+        private async Task<Product> InsertProduct(long categoryId, Product product)
+        {
+            if (categoryId <=0 )
+                return null;
+            else
+            {
+                return await _productRepo.AddProduct(product, categoryId);  
+            }
+        }
+
+        private async Task<Product> UpdateProduct(long productId, string productName)
+        {
+            if (productId <= 0)
+                return null;
+            else
+            {
+                return await _productRepo.UpdateProduct(productId, productName);
+            }
+        }
+
+
+        private async Task<bool> DeleteProduct(long productId)
+        {
+            if (productId <= 0)
+                return false;
+            else
+            {
+                return await _productRepo.DeleteProduct(productId);
             }
         }
 
