05d8a0e20818c3c03a554f89dab21f7b50be4fa4
eswaribala <parameswaribala@gmail.com>
2023-06-20 06:41:53 +0530

    updated

---
+++ InventoryAPI/InventoryAPI/Queries/CategoryGLType.cs
@@ -0,0 +1,16 @@
+﻿using GraphQL.Types;
+using InventoryAPI.Models;
+
+namespace InventoryAPI.Queries
+{
+    public class CategoryGLType:ObjectGraphType<Category>
+    {
+        public CategoryGLType()
+        {
+            Name = "category";
+            Field(_ => _.CategoryId).Description("Category Id");
+            Field(_ => _.CategoryName).Description("Category Name");
+        }
+
+    }
+}
