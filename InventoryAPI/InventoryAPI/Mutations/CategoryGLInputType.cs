7db9211985688d54e9e1c0272e926d7b4e1f0638
eswaribala <parameswaribala@gmail.com>
2023-06-20 08:57:17 +0530

    updated

---
+++ InventoryAPI/InventoryAPI/Mutations/CategoryGLInputType.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Types;
+
+namespace InventoryAPI.Mutations
+{
+    public class CategoryGLInputType:InputObjectGraphType
+    {
+        public CategoryGLInputType() {
+
+            Name = "CategoryInput";
+            Field<NonNullGraphType<LongGraphType>>("CategoryId");
+            Field<NonNullGraphType<StringGraphType>>("CategoryName");
+        
+        }
+    }
+}
