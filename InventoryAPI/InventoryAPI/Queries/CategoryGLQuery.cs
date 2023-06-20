d20a38f0f12ee3d8d30cba2341667e5863cfd924
eswaribala <parameswaribala@gmail.com>
2023-06-20 06:49:32 +0530

    updated

--- InventoryAPI/InventoryAPI/Queries/CategoryGLQuery.cs
+++ InventoryAPI/InventoryAPI/Queries/CategoryGLQuery.cs
@@ -1,6 +1,29 @@
-﻿namespace InventoryAPI.Queries
+﻿using GraphQL;
+using GraphQL.Types;
+using InventoryAPI.Repositories;
+
+namespace InventoryAPI.Queries
 {
-    public class CategoryGLQuery
+    public class CategoryGLQuery:ObjectGraphType
     {
+        public CategoryGLQuery(ICategoryRepo categoryRepo) {
+
+            Name = "Categories";
+            Field<ListGraphType<CategoryGLType>>(
+                 "All Categories",
+                 
+              resolve: context => categoryRepo.GetCategories()
+
+                ) ;
+
+            Field<CategoryGLType>(
+                "Category ById",
+                 arguments: new QueryArguments(new QueryArgument<LongGraphType>
+                 { Name = "categoryId" }),
+                resolve: context => categoryRepo.GetCategoryById(context.GetArgument<long>("categoryId"))
+               );
+        
+        
+        }
     }
 }
