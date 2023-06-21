d1ed44bbcf72b76f86c049ce05ad30eca9757142
eswaribala <parameswaribala@gmail.com>
2023-06-21 13:03:06 +0530

    updated

---
+++ CQRSAPI/CQRSAPI/Models/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace CQRSAPI.Models
+{
+    [Table("Cart")]
+    public class Cart
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("CartId")]
+        public long CartId { get; set; }
+        [Column("CartName")]
+        public string? CartName { get; set;}
+        [JsonIgnore]
+        public ICollection<Product>? ProductList;
+    }
+}
---
+++ CQRSAPI/CQRSAPI/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CQRSAPI.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("ProductId")]
+        public long ProductId { get; set; }
+        [Column("ProductName")]
+        public string? ProductName { get; set; }
+        [Column("Cost")]
+        public long Cost { get; set; }
+
+        [ForeignKey("Cart")]
+        public long CartId { get; set; }
+        public Cart? Cart { get; set; }
+
+    }
+}
