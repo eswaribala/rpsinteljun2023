61e1e290503cef9a4cb9bd25537fa017845320da
eswaribala <parameswaribala@gmail.com>
2023-06-21 13:13:48 +0530

    updated

---
+++ CQRSAPI/CQRSAPI/Contexts/CQRSContext.cs
@@ -0,0 +1,18 @@
+﻿using CQRSAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRSAPI.Contexts
+{
+    public class CQRSContext:DbContext
+    {
+        public CQRSContext(DbContextOptions<CQRSContext> options):base(options) { 
+        
+            this.Database.EnsureCreated();
+            this.Database.Migrate();
+
+        }
+
+        public DbSet<Cart> Carts { get; set; }  
+        public DbSet<Product> Products { get; set; }    
+    }
+}
