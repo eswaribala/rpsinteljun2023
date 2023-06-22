a246cf1eb8392e4232f4335e15cf3544d8ecc75d
eswaribala <parameswaribala@gmail.com>
2023-06-22 11:23:00 +0530

    updated

---
+++ InventoryAPI/InventoryAPI/Contexts/InventoryIdentityContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryAPI.Contexts
+{
+    public class InventoryIdentityContext:IdentityDbContext
+    {
+        public InventoryIdentityContext(DbContextOptions<InventoryIdentityContext> options):base(options) { 
+        
+          this.Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
